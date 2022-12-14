5bc7a35e023eb2c1109414b7c61e77bcbbf870c0
RicPra <114755903+RicPra@users.noreply.github.com>
2022-12-14 17:13:36 +0100

    Update R_code_firstExercise.r

--- R_code_firstExercise.r
+++ R_code_firstExercise.r
@@ -2,4 +2,19 @@
 install.packages("readxl")
 
 # Setting the working directory
-setwd("/Users/Ricky/Documents/UNI/GCE&SDG")
+setwd("/Users/Ricky/Documents/UNI/GCE & SDGs/LABORATORY OF PLANT CONSERVATION")
+
+# Recalling the package to read the data
+library(readxl)
+
+# Saving the data
+lamium <- read_excel("Reproductive strategies Lamium maculatum.xlsx")
+
+summary(lamium)
+attach(lamium)
+
+ratio <- Seeds/4
+
+boxplot((ratio[Test == "A"]), (ratio[Test == "SS"]), (ratio[Test == "PS"]), (ratio[Test == "N"]),
++         names=c("Agamospermy", "Spontaneous selfing", "Pollen supplementation", "Natural"),
++         main="Seeds/Tot seeds")
